d169ad836e53939cfbf51574ff39c76b31446fce
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2020-01-10 23:14:52 -0800

    7.1.5 Added ERD PNG and Text file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Pewlett-Hackard-Analysis
 PostgreSQL, table join, views, reports employee data migration and analysis
+
+## CSV Files
+departments.csv 
+dept_emp.csv
+dept_manager.csv
+employees.csv
+salaries.csv
+titles.csv
+
+## Entity Relationship Diagrams (ERDs)
+Primary keys
+Foreign keys
+Data types for each column, there are three types of ERDs: conceptual, logical, and physical
+
---
+++ students - QuickDBD-Free Diagram.sql
@@ -0,0 +1,34 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/ZKJKJL
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "students" (
+    "id" integer   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    CONSTRAINT "pk_students" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "courses" (
+    "id" integer   NOT NULL,
+    "course_name" varchar   NOT NULL,
+    CONSTRAINT "pk_courses" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "student_courses_junction" (
+    "student_id" integer   NOT NULL,
+    "course_id" integer   NOT NULL,
+    "term" varchar   NOT NULL
+);
+
+ALTER TABLE "student_courses_junction" ADD CONSTRAINT "fk_student_courses_junction_student_id" FOREIGN KEY("student_id")
+REFERENCES "students" ("id");
+
+ALTER TABLE "student_courses_junction" ADD CONSTRAINT "fk_student_courses_junction_course_id" FOREIGN KEY("course_id")
+REFERENCES "courses" ("id");
+
