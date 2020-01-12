059eaff36fa28a262abf736e2d7dbefc88523ba0
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2020-01-11 19:36:03 -0800

    7.3.1 added Retirement info data for born 1952-1953 and hired 1985-1988 41,381 rows CSV

--- queries.sql
+++ queries.sql
@@ -1,3 +1,38 @@
 -- 
 SELECT * FROM departments;
 
+select * from employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- 90,398
+SELECT count(first_name)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT count(first_name)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
--- schema.sql
+++ schema.sql
@@ -24,8 +24,8 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no),
@@ -39,9 +39,7 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no),
-  UNIQUE (title)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -50,5 +48,5 @@ CREATE TABLE salaries (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+
 );
